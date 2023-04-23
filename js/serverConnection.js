1ebd7384ca2bebc0e99c864ba42216c0e7eccaea
Daria <daria_v00@mail.ru>
2023-04-23 14:14:29 +0500

    feat: users photos filters

--- js/serverConnection.js
+++ js/serverConnection.js
@@ -1,10 +1,13 @@
 import {showAlert} from './util.js';
 
+const photoFilter = document.querySelector('.img-filters ');
+
 const getData = (onSuccess) => {
   fetch('https://26.javascript.pages.academy/kekstagram/data')
     .then((response) => response.json())
     .then((photos) => {
       onSuccess(photos);
+      photoFilter.classList.remove('img-filters--inactive');
     })
     .catch(() => {
       showAlert('Не удалось получить фотографии других пользователей(');
